ee01b298907353fdf11abefb8cc1796cec896523
Ayelen <ayelenreinglestein@gmail.com>
2026-02-20 03:48:53 -0300

    Fix: change env var name to MONGO_URI

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config()
 
-const mongo_url = (process.env.MONGO_URL)
+const mongo_uri = (process.env.MONGO_URI)
 
 export const dbConnection = async () => {
     try {
-        await mongoose.connect(mongo_url)
+        await mongoose.connect(mongo_uri)
         console.log("Base de datos conectada")
     } catch (error) {
         console.log("Error al conectar la base de datos", error)        
