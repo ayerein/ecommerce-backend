5f26fd1d8a795593ee7c117ba5312ad368993302
Ayelen <ayelenreinglestein@gmail.com>
2026-02-20 03:46:01 -0300

    Fix: change env var name to MONGO_URI

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ app.use("/api/cart", cartRoutes)
 
 app.use("/api/orders", orderRoutes)
 
-app.listen(app.get("PORT"), () => {
-  console.log(`Escuchando servidor en puerto http://localhost:${app.get("PORT")}`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto: ${PORT}`);
 });
