1b14380abaef80ad221abbdb784946f5c666ed84
Ayelen <ayelenreinglestein@gmail.com>
2026-03-25 21:29:23 -0300

    fix: ajustar transporte de nodemailer

--- src/config/mailing.js
+++ src/config/mailing.js
@@ -4,11 +4,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const transport = nodemailer.createTransport({
-    service: process.env.MAILING_SERVICE,
+    host: process.env.MAILING_SERVICE,
     port: process.env.MAILING_PORT,
     secure: true,
     auth: {
         user: process.env.MAILING_ACCOUNT,
         pass: process.env.MAILING_PASS
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 })
