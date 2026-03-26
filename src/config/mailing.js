5504d912c48ecf2844e1ccca73a44f76866a2bdc
Ayelen <ayelenreinglestein@gmail.com>
2026-03-25 22:33:04 -0300

    feat: implementar flujo de recuperación de contraseña. Integración de Nodemailer. Refactorización de persistencia usando DAOs y Servicios

--- src/config/mailing.js
+++ src/config/mailing.js
@@ -4,15 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 console.log("User:", process.env.MAILING_ACCOUNT)
 export const transport = nodemailer.createTransport({
-    host: process.env.MAILING_HOST || 'smtp.gmail.com',
+    service: process.env.MAILING_SERVICE,
     port: process.env.MAILING_PORT,
-    secure: false,
     auth: {
         user: process.env.MAILING_ACCOUNT,
         pass: process.env.MAILING_PASS
     },
     tls: {
         rejectUnauthorized: false
-    },
-    family: 4
+    }
 })
