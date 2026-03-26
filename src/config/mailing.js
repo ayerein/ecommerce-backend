84b0f951d37101328d160b2d15ac5454d4f3909e
Ayelen <ayelenreinglestein@gmail.com>
2026-03-25 22:19:51 -0300

    Fix

--- src/config/mailing.js
+++ src/config/mailing.js
@@ -4,8 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 console.log("User:", process.env.MAILING_ACCOUNT)
 export const transport = nodemailer.createTransport({
-    host: process.env.MAILING_HOST,
-    service: process.env.MAILING_SERVICE,
+    host: process.env.MAILING_HOST || 'smtp.gmail.com',
     port: process.env.MAILING_PORT,
     secure: false,
     auth: {
